26149130ff41211cbfc02641057cdab0bb07875c
LaurenOdalen <laurenodalen@gmail.com>
2022-03-07 13:17:45 -0700

    how long did that take

---
+++ howLongDidThatTake.js
@@ -0,0 +1,64 @@
+Number.prototype.isPrime = function () {
+    for (let i = 2; i < Math.sqrt(num); i++) {
+        if (this % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+const { performance } = require('perf_hooks');
+const start = performance.now();
+let primeCount = 0;
+let num = 2; // for math reasons, 1 is considered prime
+while (primeCount < 1e4) {
+    if (num.isPrime()) {
+        primeCount++;
+    }
+    num++;
+}
+console.log(`The 10,000th prime number is ${num - 1}`);
+console.log(`This took ${performance.now() - start} milliseconds to run`);
+
+
+
+
+// fib
+
+// recursive
+function rFib(n) {
+    if (n < 2) {
+        return n;
+    }
+    return rFib(n - 1) + rFib(n - 2);
+}
+rFib(20);
+
+
+
+// iterative
+function iFib(n) {
+    const vals = [0, 1];
+    while (vals.length - 1 < n) {
+        let len = vals.length;
+        vals.push(vals[len - 1] + vals[len - 2]);
+    }
+    return vals[n];
+}
+iFib(20);
+// iterative runs faster
+
+
+// reverse string
+
+const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
+// const reversed1 = story.split("").reverse().join("");
+
+function reverseStr(story) {
+    if (story === "") {
+        return "";
+    }
+    const strLessFirst = story.slice(1);
+    return reverseStr(strLessFirst) + story[0];
+}
+
+console.log(reverseStr(story));
